3e1265926c000dd9e5bf69ce5f771554d646e604
D059566 <jonathan.von.rueden@sap.com>
2023-11-13 22:55:17 +0100

    load dotenv and start client

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,25 +10,58 @@ load_dotenv()
 st.set_page_config(page_title="🤗💬 Jewel")
 
 # OpenAI API Key
+import os
+import streamlit as st
+
+load_dotenv()
+client = OpenAI()
+
+# Start of the Streamlit sidebar
 with st.sidebar:
+    # Streamlit UI setup for title
     st.markdown("""
         <h1 style="text-align: center">
-        💎💎<br>Software Aus Polen:<br>Jewel<br>💎💎
+        Software Aus Polen<br><br> <img src="https://raw.githubusercontent.com/JHFVR/jle/main/jle_blue.svg" width="50px" height="50px" /> <br><br>
         </h1>
         """, unsafe_allow_html=True)
-    if 'OPENAI_API_KEY' in os.environ:
-        st.success('OpenAI API key already provided!', icon='✅')
-        openai_api_key = os.environ['OPENAI_API_KEY']
-    else:
-        openai_api_key = st.text_input('Enter OpenAI API key:', type='password')
-        if not openai_api_key:
-            st.warning('Please enter your API key!', icon='⚠️')
+
+    # Check if running in a Cloud Foundry environment
+    if 'VCAP_SERVICES' in os.environ or 'VCAP_APPLICATION' in os.environ:
+        # Cloud Foundry specific behavior
+        if 'OPENAI_API_KEY' in os.environ:
+            st.success('OpenAI API key already provided!', icon='✅')
+            openai_api_key = os.environ['OPENAI_API_KEY']
         else:
-            # Save the API key to the .env file
-            with open('.env', 'a') as f:
-                f.write(f'OPENAI_API_KEY={openai_api_key}\n')
-            st.success('API key stored. Proceed to chat!', icon='👉')
+            openai_api_key = st.text_input('Enter OpenAI API key:', type='password')
+            if openai_api_key:
+                # Store the API key in a variable
+                instance_variable_openai_api_key = openai_api_key
+                st.success('API key stored in session for Cloud Foundry. Proceed to chat!', icon='👉')
+            else:
+                st.warning('Please enter your API key!', icon='⚠️')
+    else:
+        # Behavior for non-Cloud Foundry environments
+        if 'OPENAI_API_KEY' in os.environ:
+            st.success('OpenAI API key already provided!', icon='✅')
+            openai_api_key = os.environ['OPENAI_API_KEY']
+        else:
+            openai_api_key = st.text_input('Enter OpenAI API key:', type='password')
+            if not openai_api_key:
+                st.warning('Please enter your API key!', icon='⚠️')
+            else:
+                # Save the API key to the .env file
+                with open('.env', 'a') as f:
+                    f.write(f'OPENAI_API_KEY={openai_api_key}\n')
+                st.success('API key stored. Proceed to chat!', icon='👉')
+
+    # some more info
+    st.markdown('<br><br>', unsafe_allow_html=True)
+    
     st.markdown('📖 Here\'s where you can generate your OpenAI API key: https://help.openai.com/en/articles/4936850-where-do-i-find-my-api-key')
+
+    st.markdown('<br>', unsafe_allow_html=True)
+
+    st.markdown('🐙 Github: Here\'s the link to the project on Github: https://github.com/JHFVR/glowing-octo-enigma/')
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
@@ -36,7 +69,8 @@ if "messages" not in st.session_state.keys():
 
 # Display chat messages
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
+    role = message["role"]
+    with st.chat_message(role, avatar='https://raw.githubusercontent.com/JHFVR/jle/main/jle_blue.svg' if role == "assistant" else None):
         st.write(message["content"])
 
 # Function for generating LLM response
@@ -57,7 +91,7 @@ if prompt := st.chat_input(disabled=not openai_api_key):
 
 # Generate a new response if last message is not from assistant
 if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
+    with st.chat_message("assistant", avatar='https://raw.githubusercontent.com/JHFVR/jle/main/jle_blue.svg'):
         with st.spinner("Thinking..."):
             response = generate_response(prompt, openai_api_key) 
             st.write(response) 
